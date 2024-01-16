bb691619f4b5b5056f17ea29bcf02fdff33f5004
firasyazid <98762038+firasyazid@users.noreply.github.com>
2024-01-16 09:37:11 +0100

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,373 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  rest,
+  fin
+    
+} from "../assets";
+ 
+export const navLinks = [
+  {
+    id: "about",
+    title: "À propos",
+  },
+  {
+    id: "work",
+    title: "Projets",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  
+  {
+    title: "Création des supports graphique",
+    icon: mobile,
+  },
+  {
+    title: "Création du contenu pour les réseaux sociaux",
+    icon: backend,
+  },
+  {
+    title: "Wordpress",
+    icon: web,
+  },
+];
+
+const technologies = [
+ 
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+ 
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+];
+
+const experiences = [
+  {
+    title: "Formation Community Manager ",
+     icon: starbucks,
+    iconBg: "#E6DEDD",
+    date: "Depuis Septembre 2023",
+    points: [
+
+      "En tant que Community Manager , j'essaie d'établir une présence digitale robuste et créer une communauté engagée autour d'une  marque. La formation a été cruciale pour affiner mes compétences en gestion de communauté, élaboration de stratégies de contenu et optimisation de la visibilité en ligne. Ainsi, la fusion de mon expertise graphique et des compétences de community management a tracé la voie vers un avenir prometteur ouvrant la porte à de nouvelles opportunités"
+       ],
+  },
+  {
+    title: "Graphiste",
+    company_name: "Auto-entreprise",
+    icon: starbucks,
+    iconBg: "#E6DEDD",
+    date: "Depuis 2020",
+    points: [
+      "Le graphisme est l'art de concevoir des élements visuels tels que les images ,textes, couleurs. Graphiste de métier, je lance mon projet <<Art'emis>> cette marque consiste à créer tout type de support graphique pour les entreprises et les particuliers."
+       ],
+  },
+
+ 
+
+  {
+    title: "Graphiste",
+    company_name: "Techno Pub",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: " Depuis 2015  ",
+    points: [
+      "En tant que graphiste dans cette agence,j'ai acquis une solide expérience. J'ai également une excellente maîtrise de l'outil informatique et des logiciels de création graphique, notamment InDesign, Illustrator et Photoshop, ainsi que des outils de production tels que les imprimantes et les techniques d'impression."
+      ],
+  },
+   
+];
+
+const testimonials = [
+  
+  
+];
+
+const projects = [
+  {
+    name: "Logo et papeterie TechnoPub",
+    description:
+ "En tant que graphiste, j'ai eu le privilège de travailler sur le projet de conception du logo et de la papeterie pour TechnoPub. L'objectif était de créer une identité visuelle distinctive et professionnelle reflétant la nature technologique et innovante de l'entreprise."    ,
+     tags: [
+      {
+        name: "photoshop",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "adobe  illustrator",
+        color: "green-text-gradient",
+      },
+      
+    ],
+    image: carrent,
+   },
+
+
+  {
+    name: "Logo et papeterie Atelier Jesmonite",
+    description:
+    "En tant que graphiste, j'ai eu le plaisir de concevoir le logo et papeterie pour l'Atelier Jasmin. L'objectif de ce projet était de créer une identité visuelle unique et représentative pour cet atelier.",
+
+     tags: [
+      {
+        name: "photoshop",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "adobe  illustrator",
+        color: "green-text-gradient",
+      },
+      
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/firasyazid/AngularProject",
+  },
+
+
+  {
+    name: "Logo et papeterie Otizm",
+    description:
+
+    "Ce projet a été une opportunité gratifiante de contribuer à la représentation visuelle d'une organisation dédiée à l'inclusion et au soutien des personnes touchées par l'autisme. Le logo et la papeterie sont conçus pour refléter les valeurs et la mission essentielles de l'Association Otizm. ",
+  tags: [
+  {
+    name: "photoshop",
+    color: "blue-text-gradient",
+  },
+  {
+    name: "adobe  illustrator",
+    color: "green-text-gradient",
+  },
+  
+],
+    image: tripguide,
+   },
+
+   {
+    name: "Conception du Menu Restaurant",
+    description:
+
+ "En tant que graphiste, j'ai eu l'occasion de concevoir un menu pour un restaurant, créant ainsi une expérience visuelle qui reflète le style, l'ambiance et la cuisine unique de l'établissement.",
+   tags: [
+  {
+    name: "photoshop",
+    color: "blue-text-gradient",
+  },
+  {
+    name: "adobe  illustrator",
+    color: "green-text-gradient",
+  },
+  
+],
+    image: redux,
+   },
+   {
+    name: "Conception du Menu Restaurant",
+    description:
+
+"Ce projet de conception de menu a été une opportunité passionnante de fusionner créativité et fonctionnalité pour représenter visuellement l'identité gastronomique distinctive du restaurant.",
+   tags: [
+  {
+    name: "photoshop",
+    color: "blue-text-gradient",
+  },
+  {
+    name: "adobe  illustrator",
+    color: "green-text-gradient",
+  },
+  
+],
+    image: tailwind,
+   },
+
+   {
+    name: "Conception du Menu Restaurant",
+    description:
+
+"Ce projet de conception de menu a été une opportunité passionnante de fusionner créativité et fonctionnalité pour représenter visuellement l'identité gastronomique distinctive du restaurant.",
+   tags: [
+  {
+    name: "photoshop",
+    color: "blue-text-gradient",
+  },
+  {
+    name: "adobe  illustrator",
+    color: "green-text-gradient",
+  },
+  
+],
+    image: docker,
+   },
+   {
+    name: "Emballages  et étiquettes produits",
+    description:
+        "Ce projet a été une opportunité excitante de marier l'esthétique créative à la fonctionnalité pratique, visant à attirer l'attention des consommateurs tout en fournissant des informations claires sur les produits de la gamme de masques capillaires.",
+
+    tags: [
+  {
+    name: "photoshop",
+    color: "blue-text-gradient",
+  },
+  {
+    name: "adobe  illustrator",
+    color: "green-text-gradient",
+  },
+  
+],
+    image: figma,
+   },
+
+   {
+    name: "Emballages  et étiquettes produits",
+    description:
+ "Ce projet a été une expérience passionnante, mettant en œuvre des éléments visuels qui captent l'essence même de l'huile d'argan, tout en créant un design d'emballage attrayant et informatif pour attirer les consommateurs. ",
+    tags: [
+  {
+    name: "photoshop",
+    color: "blue-text-gradient",
+  },
+  {
+    name: "adobe  illustrator",
+    color: "green-text-gradient",
+  },
+  
+],
+    image: tesla,
+   },
+
+
+   {
+    name: "Emballages  et étiquettes produits",
+    description:
+ "Ce projet a été une expérience passionnante, mettant en œuvre des éléments visuels qui captent l'essence même de l'huile d'argan, tout en créant un design d'emballage attrayant et informatif pour attirer les consommateurs. ",
+    tags: [
+  {
+    name: "photoshop",
+    color: "blue-text-gradient",
+  },
+  {
+    name: "adobe  illustrator",
+    color: "green-text-gradient",
+  },
+  
+],
+    image: meta,
+   },
+
+
+   {
+    name: "Contenu réseau sociaux",
+    description:
+
+    "J'ai eu le privilège de concevoir et mettre en œuvre une campagne visuelle captivante pour les soldes estivales , Chaque publication a été pensée pour refléter l'esthétique de la boutique tout en transmettant l'esprit de la saison des soldes.",
+    tags: [
+  {
+    name: "photoshop",
+    color: "blue-text-gradient",
+  },
+  {
+    name: "adobe  illustrator",
+    color: "green-text-gradient",
+  },
+  
+],
+    image: creator,
+   },
+
+   {
+    name: "Contenu réseau sociaux",
+    description:
+
+    "Explorez ci-dessous quelques-unes de mes réalisations en tant que responsable des réseaux sociaux, illustrant comment une présence numérique forte peut jouer un rôle essentiel dans la promotion d'un restaurant et dans l'interaction avec une clientèle toujours plus connectée. ",
+
+     tags: [
+  {
+    name: "photoshop",
+    color: "blue-text-gradient",
+  },
+  {
+    name: "adobe  illustrator",
+    color: "green-text-gradient",
+  },
+  
+],
+    image: rest,
+   },
+
+   {
+    name: "Contenu réseau sociaux",
+    description:
+
+    ". Explorez ci-dessous quelques-unes de mes réalisations en tant que stratège des médias sociaux, soulignant l'efficacité d'une stratégie numérique bien orchestrée dans le monde du DJing.",
+
+     tags: [
+  {
+    name: "photoshop",
+    color: "blue-text-gradient",
+  },
+  {
+    name: "adobe  illustrator",
+    color: "green-text-gradient",
+  },
+  
+],
+    image: fin,
+   },
+];
+
+export { services, technologies, experiences, testimonials, projects };
